31c9612033911b93d88d3d220cd4daf0aff3f043
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-11 12:38:35 -0500

    Se creo la clase CSedan y se le agrego la funcionalidad correspondiente

---
+++ Proyecto1erParcialAutos/Proyecto1erParcialAutos/CSedan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1erParcialAutos
+{
+    internal class CSedan : CAuto
+    {
+        public void estable()
+        {
+            Console.WriteLine("Tiene una conducción estable y comoda gracias a la distribución del peso.");
+        }
+
+        public CSedan(string nombreSedan) : base(nombreSedan)
+        {
+
+        }
+
+        public override void caracteristicas()
+        {
+            Console.WriteLine("Son la perfecta combinación entre comodidad, seguridad y deportivismo, es la combinación entre un auto familiar y un auto deportivo. Suelen ahorrar mas combustible que el resto");
+        }
+    }
+}
