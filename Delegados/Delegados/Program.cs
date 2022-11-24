257e617380f6e220c95753ddb6725849b74d8d7f
Emigdio Espinosa <millosespinosa@gmail.com>
2022-11-24 09:57:44 -0600

    Hicimos la practica de delegados

---
+++ Delegados/Delegados/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Delegados
+{
+    internal class Program
+    {
+        delegate void objetoDelegado(string msj);
+        static void Main(string[] args)
+        {
+            //Creamos el objeto delegado que apunta a la clase MensajeBienvenida 
+            objetoDelegado miDelegado = new objetoDelegado(MensajeBienvenida.saludoBienvenida);
+
+            //Utilización de delegado para llamar al metodo saludoBienvenida
+            miDelegado("Hola estimados estudiantes");
+
+            Console.ReadLine();
+
+            objetoDelegado miDelegado2 = new objetoDelegado(MensajeDespedida.saludoDespedida);
+            miDelegado2("Bye estimados estudiantes");
+            Console.ReadLine();
+        }
+    }
+
+    class MensajeBienvenida
+    {
+        public static void saludoBienvenida(string msj)
+        {
+            Console.WriteLine("Hola estimados estudiantes, sean bienvenidos"); 
+        }
+    }
+
+    class MensajeDespedida
+    {
+        public static void saludoDespedida(string msj)
+        {
+            Console.WriteLine("Bye estimados estudiantes, bonito dia");
+        }
+    }
+}
